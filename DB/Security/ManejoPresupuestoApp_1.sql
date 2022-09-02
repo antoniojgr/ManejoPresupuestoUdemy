c3a6eee1cab45643b8f0223f39293aac56a9a0af
Antonio Javier Garrido Rodriguez <antoniojaviergr@gmail.com>
2022-09-02 14:41:58 +0200

    correccion

--- DB/Security/ManejoPresupuestoApp_1.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE LOGIN [ManejoPresupuestoApp]
-    WITH PASSWORD = N'LrqeyjxbsygqylJycwvxrh8rmsFT7_&#$!~<ruriYwfexd{v', SID = 0x47ABA3C3D9AB7C4DBF617977125A8648, DEFAULT_DATABASE = [ManejoPresupuesto], DEFAULT_LANGUAGE = [Español], CHECK_POLICY = OFF;
-
